d77d66af620fdad77842dfbc7458b53421e89ce5
santimncd-hue <santimncd@gmail.com>
2026-05-16 16:45:44 +0000

    avance un poquito

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -2,7 +2,10 @@ package com.example;
 
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
-        // TODO
+        int edad = 20;
+        double estatura = 1.8;
+        System.out.println(edad);
+        System.out.println(estatura);
     }
 
     public static void demostrarConvencionesNombres() {
